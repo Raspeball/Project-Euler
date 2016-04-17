99437405a702460d0130ee41747d7e063bdd3ff3
Raspeball <larrstav@gmail.com>
2016-04-18 01:42:03 +0200

    Some changes
    Fixing the error in computing triangular numbers (trianglenum).
    Improving the code by redusing the number of numbers to search in
    finding divisors - using the fact that no divisor is larger than num/2.

--- projecteuler12.py
+++ projecteuler12.py
@@ -1,29 +1,34 @@
 def trianglenum(n): #computes the n-th triangle number
-	trinum = n*(n+1)
+	trinum = (n*(n+1))/2
 	return trinum
 
 def divnum(x):
-	d = 0
-	for i in range(1,(x+1)):
-		if x % i == 0:
-			d = d +1
-		else:
-			continue
+	d = 1 # start at 1 since every number has itself as divisor
+	if x % 2 == 0:
+		for i in range(1,((x/2)+1)):
+			if x % i == 0:
+				d = d+1
+			else:
+				continue
+	else:
+		for i in range(1,((x+1)/2)+1):
+			if x % i == 0:
+				d = d + 1
+			else:
+				continue
 	return d
 
 
 def main():
-	tnums = 8
+	tnums = 14000 # #-th triangular number
 	while tnums > 1:
 		triangle = trianglenum(tnums)
-		trianglediv = divnum(tnums)
-		if trianglediv >= 500:
-			return triangle
+		trianglediv = divnum(triangle)
+		if trianglediv >= 300:
 			break
 		else:
-			tnums = tnums +1
-	
-	print triangle
+			tnums = tnums + 1
+		#
+	return triangle
 
-main()
-		
+print(main())
