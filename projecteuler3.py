9ae65229e2eaf414b640a4fc629023910c0fb04a
Raspeball <larrstav@gmail.com>
2016-04-18 01:45:27 +0200

    Minor changes

---
+++ projecteuler3.py
@@ -0,0 +1,4 @@
+eksnum = 13195
+eksnumprimes = (5,7,13,29)
+
+num = 600851475143
--- projecteuler5.py
+++ projecteuler5.py
@@ -9,5 +9,15 @@ def OneTwentyEvenlyDivisible():
 				break
 			else:
 				startnum = startnum + 1
+	return startnum
 
-OneTwentyEvenlyDivisible()
+#OneTwentyEvenlyDivisible()
+
+#Does not need any code to do this, really.
+# 2520 is divisible with numbers 1-10
+# Then, you need a factor 2 for 20 and 16 (2*8), and then the primes 11,13,17,19
+# Done!
+
+ans = 2520*2*11*13*17*19
+
+print(ans)
