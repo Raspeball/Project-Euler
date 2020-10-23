da3e411068cbdcbe61909ac02639340047578e5a
Raspeball <Raspeball@users.noreply.github.com>
2020-10-24 01:22:11 +0200

    Solution 22
    Solved problem 22

--- projecteuler22.py
+++ projecteuler22.py
@@ -1,25 +1,32 @@
-f = open("p022_names.txt","r")
+f = open("p022_names.txt", "r")
 #
 for names in f:
     name_list = names.split(",")
-    #
-#
-name_list.sort()
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-alfadic = {}
-for i in range(len(alphabet)):
-    alfadic[alphabet[i]]=i+1
-    #
-#
-for j in range(len(name_list):
-    for k in range(len(name_list[j])):
-               
-#
-#
-#
-## --- Test area --- ##
-#print(alfadic.keys())
-#print(alfadic.values())
-#print(name_list)
-## --- --- ##
 
+f.close()
+
+name_list.sort()
+
+for n in range(len(name_list)):
+    name_list[n] = name_list[n].strip('"')
+
+print(name_list)
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def AlphaSum(name):
+
+    sum = 0
+    for i in name:
+        sum += (alphabet.index(i) + 1)
+
+    return sum
+
+print(AlphaSum(name_list[0]))
+
+total_name_score = 0
+
+for n in range(len(name_list)):
+    total_name_score += (AlphaSum(name_list[n])*(n + 1))
+
+print(total_name_score)
