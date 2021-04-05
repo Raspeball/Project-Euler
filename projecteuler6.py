be805ad844e39f88718528f15f1590843993ca82
Raspeball <Raspeball@users.noreply.github.com>
2021-04-05 18:58:40 +0200

    Py2 --> Py3. Now uses functions.

--- projecteuler6.py
+++ projecteuler6.py
@@ -1,13 +1,25 @@
-hundre = range(1,101)
+# Using the Gauss-formula (triangle numbers) to find the square of the sum #
 
-sqsum = ((101*100)/2)**2
-sumsq = 0
+def SquareOfSum(n):
+	# n is int
+	# finds the square of the sum of the n first natural numbers
+	res = ((n * (n + 1)) / 2)**2
+	return res
 
-for i in range(len(hundre)):
-	sumsq += hundre[i]**2
 
-diff = sqsum - sumsq
+def SumOfSquares(n):
+	# n is int
+	# range is ok since 0 does not add to res
+	# finds the sum of the square of the n first natural numbers
+	res = 0
+	for i in range(n + 1):
+		res += i**2
 
-print diff
+	return res
 
-		
+target = 100 # we want to work with the first 100 natural numbers
+
+diff = SquareOfSum(target) - SumOfSquares(target)
+
+# print result
+print(diff)
