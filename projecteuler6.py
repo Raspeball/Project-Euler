6ce0416f8cf3333ce82ff50ffc033c5ecb75df4e
Raspeball <Raspeball@users.noreply.github.com>
2021-04-05 19:14:07 +0200

    Reverse order of functions

--- projecteuler6.py
+++ projecteuler6.py
@@ -1,12 +1,3 @@
-# Using the Gauss-formula (triangle numbers) to find the square of the sum #
-
-def SquareOfSum(n):
-	# n is int
-	# finds the square of the sum of the n first natural numbers
-	res = ((n * (n + 1)) / 2)**2
-	return res
-
-
 def SumOfSquares(n):
 	# n is int
 	# range is ok since 0 does not add to res
@@ -16,6 +7,15 @@ def SumOfSquares(n):
 		res += i**2
 
 	return res
+
+
+# Using the Gauss-formula (triangle numbers) to find the square of the sum #
+def SquareOfSum(n):
+	# n is int
+	# finds the square of the sum of the n first natural numbers
+	res = ((n * (n + 1)) / 2)**2
+	return res
+
 
 target = 100 # we want to work with the first 100 natural numbers
 
