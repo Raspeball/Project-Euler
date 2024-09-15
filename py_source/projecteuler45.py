7a3421a3f922897ca44d72d895685fa03e79bd09
Raspeball <Raspeball@users.noreply.github.com>
2024-09-15 19:26:52 +0200

    Update projecteuler45.py

--- py_source/projecteuler45.py
+++ py_source/projecteuler45.py
@@ -2,14 +2,17 @@ import math
 
 def Trianglenum(n):
     res = (n*(n+1))/2
+    
     return res
 
 def Pentagonalnum(k):
     res = (k*(3*k - 1))/2
+    
     return res
 
 def Hexagonalnum(j):
     res = j*(2*j - 1)
+    
     return res
 
 for h in range(144, 1000):
